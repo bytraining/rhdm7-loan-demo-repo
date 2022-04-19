4923df4c6bb27718c37371d7865f6c8063870aaf
rhdmAdmin <>
2022-04-19 15:24:00 +0800

    {/src/main/java/com/redhat/demos/dm/loan/model/Applicant.java}

--- src/main/java/com/redhat/demos/dm/loan/model/Applicant.java
+++ src/main/java/com/redhat/demos/dm/loan/model/Applicant.java
@@ -5,7 +5,7 @@ package com.redhat.demos.dm.loan.model;
  */
 
 /* add comment by boris test */
-/* add comment by boris test */
+/* add comment by boris test 2 */
 
 public class Applicant implements java.io.Serializable {
 
